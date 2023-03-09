8fe388f64b6803e83b1c6e6401893e157d11c3ab
Damien Henken <dhenken@gmail.com>
2023-03-09 04:30:07 +0000

    Update project IDs and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "gitops-380103-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="gitops-380103"
